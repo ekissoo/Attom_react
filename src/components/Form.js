84a5a59085995db6dc46639c9583535dd4542ca9
Anupam Sharma <sharmanupam2100@gmail.com>
2024-03-15 22:38:15 +0530

    change

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,7 +12,9 @@ import pluralize, { plural } from "pluralize";
 // const REST_API_URL="http://192.168.1.23:5000/";
 // const REST_API_URL="http://192.168.1.39:8080";
 // const REST_API_URL="http://localhost:8080";
-const REST_API_URL="http://ec2-35-89-18-154.us-west-2.compute.amazonaws.com:8080";
+// const REST_API_URL="https://localhost:8080";
+
+const REST_API_URL="https://ec2-35-89-18-154.us-west-2.compute.amazonaws.com:8080";
 
 // const REST_API_URL="https://d69qhe0538.execute-api.ap-south-1.amazonaws.com/";
 
