7c301396dcba54bdf3570275ae7c44754e208b0d
Anupam Sharma <sharmanupam2100@gmail.com>
2024-03-18 17:07:32 +0530

    localtunnel

--- src/components/Form.js
+++ src/components/Form.js
@@ -14,8 +14,9 @@ import pluralize, { plural } from "pluralize";
 // const REST_API_URL="http://localhost:8080";
 // const REST_API_URL="https://localhost:8080";
 
-const REST_API_URL="http://ec2-35-89-18-154.us-west-2.compute.amazonaws.com:8080";
+// const REST_API_URL="http://ec2-35-89-18-154.us-west-2.compute.amazonaws.com:8080";
 // const REST_API_URL="https://23c9-35-89-18-154.ngrok-free-app";
+const REST_API_URL="https://tender-moons-guess.loca.lt";
 
 // const REST_API_URL="https://d69qhe0538.execute-api.ap-south-1.amazonaws.com/";
 
